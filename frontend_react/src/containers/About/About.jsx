91caf25d8b57177a17cd7008e0ec98791025086c
Ralstoon Dsouza <megagravity.dsz@gmail.com>
2022-03-09 17:18:41 +0530

    Co-authored-by: YBSuperAwesome02 <YBSuperAwesome02@users.noreply.github.com>

--- frontend_react/src/containers/About/About.jsx
+++ frontend_react/src/containers/About/About.jsx
@@ -1,11 +1,16 @@
 //CODE BY RalDSZ
 //Copyright :ORBT TECHNOLOGIES all rights reserved
 //Last modified : 08/03/2022
-import React from 'react'
-import './About.scss'    
+import React,{useState, useEffect} from 'react'
+import { motion } from "framer-motion"
+
+import './About.scss' 
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <h2 className="head-text"> I am <span>Nothing</span><br/>Without<span> Hardwork</span></h2>
+    </>
   )
 }
 
--- frontend_react/src/containers/Header/Header.jsx
+++ frontend_react/src/containers/Header/Header.jsx
@@ -1,9 +1,84 @@
+//CODE BY RalDSZ
+//Copyright :ORBT TECHNOLOGIES all rights reserved
+//Last modified : 08/03/2022
 import React from 'react'
+import './Header.scss'
+import { motion } from 'framer-motion' 
+import human from '../../assets/human.png'
+import circle from '../../assets/circle.svg' 
+import img1 from './t1.png' 
+import img2 from './t2.png'
+import img3 from './t3.png'  
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+
+    }
+  }
 }
+
+const Header = () => (
+  <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-150, 0], opacity: [0, 1] }}
+      transition={{ duration: 1 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">HENLO FRENS ME IS</p>
+            <h1 className="head-text">idk</h1>
+          </div>
+        </div>
+
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Senior Project Research Scientis</p>
+          <p className="p-text">PRO</p>
+        </div>
+      </div>
+    </motion.div>
+   <motion.div
+      whileInView={{  opacity: [0, 1] }}
+      transition={{ duration: 1, delayChildren:0.5}}
+      className="app__header-img"
+    >
+      <img src={human} alt="human" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={circle}
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[img1, img2, img3].map((circle) => (
+        <div className="circle-cmp app__flex" key={`circle`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
+  </div>
+    
+
+    
+
+   
+     
+
+    
+ 
+);
 
 export default Header
