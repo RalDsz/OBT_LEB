1360101f0db746d02c139de9ed2b3621ec9a4080
Ralstoon Dsouza <megagravity.dsz@gmail.com>
2022-05-06 10:21:02 +0530

    Minor Changes and UI Upgrades

--- frontend_react/src/containers/Work/Work.jsx
+++ frontend_react/src/containers/Work/Work.jsx
@@ -1,11 +1,23 @@
-//CODE BY RalDSZ
-//Copyright :ORBT TECHNOLOGIES all rights reserved
-//Last modified : 08/03/2022
-import React from 'react'
-import './Work.scss'    
+import React from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import abt from './tesimg.jpg'
+import './Work.scss';
+
+const abouts = [
+    { title: 'Researcher ', description: 'I have researched in  the area of wireless and optical communication systems', imgUrl: abt },
+    { title: 'Faculty', description: 'Experience of 12 years teaching undergraduate and postgraduate courses', imgUrl: abt },
+    { title: 'Hands-on experience', description: ' 5+ years working with high-speed instruments and components for demonstrating optical coherent links, and testing analog chips integrated with respective systems', imgUrl: abt },
+    { title:'Current', description: 'Currently a working group member for an ongoing work for an IEEE Standard related to rural area network', imgUrl: abt },
+  ];
+
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+        <h2 className="head-text"> My Creative <span> Portfolio </span> </h2>
+
+    </>
   )
 }
 
