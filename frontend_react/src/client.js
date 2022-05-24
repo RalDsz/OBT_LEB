b23aa4843df06eddc630b67c0073248718d5fd26
Ralstoon Dsouza <megagravity.dsz@gmail.com>
2022-05-24 13:05:17 +0530

    Minor changes to fix Saqnity issue

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import ImgUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-    projectId: 't6nwmhla',
+    projectId: 'qb8937t7',
     dataset: "production",
     apiVersion: "2022-02-01", // add the Sanity API version you wish to use
     useCdn: true,
-    token: 'skR0yPGv0IQm5nXKh343vhnwuy2SO7KMaeT5R285VrDSecAN69y2yoyFPbAdY14PTgC9fU0i3PUxGNNxpZuObm8oZCBj7M4f25esl03W4lE0ai2MOuGoIRbLAwVoJDZLThLEtVlZQeVybzEmXoX2Jz4lcYy7nIRGSRSH6Je1hzGLow9ohK69'// or leave blank to be anonymous user
+    token: 'sko1Pw0aDi7BhizVXQblSg0HqWhKfiFLFLa0AV7053x0yGLnTsgjgAyYzTQfu3mWU9zGh4AJqisVZX0PwG3Y9JWHVyYwOk1UBJdPMWlMO7Ax3K3Ofn0QV6qamszAN1C7LlcxrszBjUPoduvGEd1TLjHBZzgI0050W2y8m3akZmbIStAzEYGd'// or leave blank to be anonymous user
 });
 
 const builder = ImgUrlBuilder(client);
